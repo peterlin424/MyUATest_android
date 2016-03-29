4b03e4466d5c79f41c754c9cd54f590a0c7b2dcf
peterlin424 <peterlin424@gmail.com>
2016-03-29 19:39:30 +0800

    test ver1

--- app/src/main/java/com/example/peter/myuatest_android/Bridge_Index.java
+++ app/src/main/java/com/example/peter/myuatest_android/Bridge_Index.java
@@ -1,26 +1,52 @@
 package com.example.peter.myuatest_android;
 
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.unity3d.player.UnityPlayerActivity;
+
 
 /**
  * Created by linweijie on 3/25/16.
  */
 public class Bridge_Index extends UnityPlayerActivity {
 
+    public static Activity activity = null;
+    @Override
+    public PendingIntent createPendingResult(int requestCode, Intent data, int flags) {
+        return super.createPendingResult(requestCode, data, flags);
+    }
+
     @Override
     protected void onCreate(Bundle bundle) {
+        Log.d("Bridge_Index", "-- onCreate --");
         super.onCreate(bundle);
     }
 
-    protected void StartActivityByUnity(){
+    protected void StartActivityByUnity(final Activity activity_unity){
+        Log.d("Bridge_Index", "-- StartActivityByUnity --");
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(Bridge_Index.this, MainActivity.class));
-                Bridge_Index.this.finish();
+                activity = activity_unity;
+                
+                Log.d("Bridge_Index", "+++ start activity : MainActivity.class");
+                activity_unity.startActivity(new Intent(activity_unity, MainActivity.class));
+            }
+        });
+    }
+
+    protected void ToastMessage(final Activity activity, final String message){
+        Log.d("Bridge_Index", "-- ToastMessage --");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+                Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/example/peter/myuatest_android/MainActivity.java
+++ app/src/main/java/com/example/peter/myuatest_android/MainActivity.java
@@ -1,52 +1,50 @@
 package com.example.peter.myuatest_android;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.unity3d.player.UnityPlayer;
 
+public class MainActivity extends Activity {
+
+    public static Activity activity = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        activity = this;
+
+        Button bt_go_page1 = (Button)findViewById(R.id.button1);
+        Button bt_go_page2 = (Button)findViewById(R.id.button2);
+
+        bt_go_page1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+//                UnityPlayer.UnitySendMessage("GameObjectName", "MethodName", "parameters");
+                UnityPlayer.UnitySendMessage("Main Camera", "ToastMessage", "goUnityPage1 is called");
+                UnityPlayer.UnitySendMessage("Main Camera", "goUnityPage1", "");
+                finish();
+            }
+        });
+
+        bt_go_page2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                UnityPlayer.UnitySendMessage("GameObjectName", "MethodName", "parameters");
+                UnityPlayer.UnitySendMessage("Main Camera", "ToastMessage", "goUnityPage2 is called");
+                UnityPlayer.UnitySendMessage("Main Camera", "goUnityPage2", "");
+                finish();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public void onBackPressed() {
+//        if (Bridge_Index.UnityActivity!=null){
+//            Bridge_Index.UnityActivity.finish();
+//        }
+//    }
 }
